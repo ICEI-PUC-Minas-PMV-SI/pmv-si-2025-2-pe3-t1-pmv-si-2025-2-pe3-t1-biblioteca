09ae0294de39594d9e8030fbdd9e6c5b1bf09286
Danilo Brito <danilo.brito.1562234@sga.pucminas.br>
2025-10-14 13:33:10 -0300

    Main

--- src/js-scripts/acervo.js
+++ src/js-scripts/acervo.js
@@ -1,11 +1,30 @@
-const selectSearch = document.getElementById("selectSearch")
-const tipoDeBusca = document.getElementById("tipo-de-busca")
-const listHidden = document.getElementById("list-hidden")
+const selectSearch = document.getElementById("selectSearch");
+const tipoDeBusca = document.getElementById("tipo-de-busca");
+const listHidden = document.getElementById("list-hidden");
+const listItems = document.querySelectorAll(".listItem");
 
 tipoDeBusca.addEventListener("click", (event) => {
-    event.preventDefault()
+  event.preventDefault();
 
-    listHidden.classList.toggle("hidden")
-})
+  listHidden.classList.toggle("hidden");
+});
 
+for (const item of listItems) {
+  item.addEventListener("click", (event) => {
+    event.preventDefault();
 
+    listHidden.classList.toggle("hidden");
+
+    selectSearch.textContent = item.textContent;
+
+    listItems.forEach((li) => {
+      li.style.opacity = li == item ? 1.0 : 0.7;
+    });
+  });
+
+  if (item.textContent === selectSearch.textContent) {
+      item.style.opacity = 1.0;
+    } else {
+        item.style.opacity = 0.7
+    }
+}
