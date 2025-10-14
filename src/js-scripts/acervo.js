44f6c9b217c5eda5321d1e0a5c7ceead65538d3d
Danilo Brito <danilo.brito.1562234@sga.pucminas.br>
2025-10-14 08:29:38 -0300

    JS Acervo

--- src/js-scripts/acervo.js
+++ src/js-scripts/acervo.js
@@ -1,3 +1,11 @@
-const selectSearch = document.querySelectorAll("selectSearch")
+const selectSearch = document.getElementById("selectSearch")
+const tipoDeBusca = document.getElementById("tipo-de-busca")
+const listHidden = document.getElementById("list-hidden")
 
-console.log(selectSearch.textContent)
+tipoDeBusca.addEventListener("click", (event) => {
+    event.preventDefault()
+
+    listHidden.classList.toggle("hidden")
+})
+
+
