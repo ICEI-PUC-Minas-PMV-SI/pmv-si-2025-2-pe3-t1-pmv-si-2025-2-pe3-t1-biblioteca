7d4a039d7eefe849f85ef4f4f009d29825c88b59
Danilo Brito <danilo.brito.1562234@sga.pucminas.br>
2025-10-29 00:58:19 -0300

    Atualização do estilo e funcionamento inicial do botão de reserva.

---
+++ src/js-scripts/livro.js
@@ -0,0 +1,8 @@
+const reserveButton = document.getElementById("livro-reservar")
+const selectButton = document.getElementById("livro-selecionar")
+
+reserveButton.addEventListener("click", (event) => {
+    event.preventDefault();
+
+
+})
