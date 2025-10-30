e5802d556cae7626937376749eb1d0597a5d5fdb
Danilo Brito <danilo.brito.1562234@sga.pucminas.br>
2025-10-29 23:34:18 -0300

    Main

--- src/js-scripts/livro.js
+++ src/js-scripts/livro.js
@@ -2,6 +2,8 @@ const reserveContainer = document.querySelector(".reserve-container")
 const reserveButton = document.getElementById("livro-reservar")
 const selectButton = document.getElementById("livro-selecionar")
 const allContainers = document.querySelectorAll(".opacity05")
+const buttonCancelReserve = document.getElementById("botao-cancelar-reserva")
+const buttonConfirmReserve = document.getElementById("botao-confirmar-reserva")
 
 reserveButton.addEventListener("click", (event) => {
     event.preventDefault();
@@ -9,5 +11,14 @@ reserveButton.addEventListener("click", (event) => {
     reserveContainer.style.display = "flex";
     allContainers.forEach(container => {
         container.style.opacity = "0.2"
+    })
+})
+
+buttonCancelReserve.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    reserveContainer.style.display = "none";
+    allContainers.forEach(container => {
+        container.style.opacity = "1"
     })
 })
