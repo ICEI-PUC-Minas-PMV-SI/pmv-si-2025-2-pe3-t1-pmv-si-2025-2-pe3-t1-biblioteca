2e9ff07b3751b1483db6103b045778b76af09c42
Danilo Brito <danilo.brito.1562234@sga.pucminas.br>
2025-11-04 00:06:12 -0300

    Atualização na função de especificação de reserva do usuário

--- src/js-funcoes/funcao-criar-minhas-reservas.js
+++ src/js-funcoes/funcao-criar-minhas-reservas.js
@@ -2,8 +2,78 @@ import { ClasseLeitor } from "../js-classes/classe-leitor.js";
 import { ClasseReserva } from "../js-classes/classe-reserva.js";
 import { ClasseLivro } from "../js-classes/classe-livro.js";
 
+const mensagemPadraoReserva = document.getElementById("mensagem-padrao-reserva")
+
 export function criarMinhasReservas(livro, leitor, reserva) {
 
-    console.log("O livro é:", livro.titulo, "do autor", livro.autor)
+    const main = document.querySelector("main")
+
+    mensagemPadraoReserva.style.display = "none"
+
+    const containerReserva = document.createElement("section")
+    containerReserva.classList.add("container-reserva")
+
+    const containerCapa = document.createElement("section")
+    containerCapa.classList.add("container-capa")
+
+    const imagemCapa = document.createElement("img")
+    imagemCapa.src = `../img/capas/${livro.capaId}.jpg`
+    imagemCapa.alt = livro.altCapa
+    imagemCapa.classList.add("capa")
+    imagemCapa.id = livro.capaId
+
+    const containerInformacaoLivro = document.createElement("section")
+    containerInformacaoLivro.classList.add("container-informacoes-livro")
+
+    const tituloDoLivro = document.createElement("h3")
+    tituloDoLivro.classList.add("titulo")
+    tituloDoLivro.textContent = livro.titulo
+
+    const containerAutorEGenero = document.createElement("div")
+    containerAutorEGenero.classList.add("autor-e-genero")
+
+    const autorDoLivro = document.createElement("span")
+    autorDoLivro.classList.add("autor")
+    autorDoLivro.title = "Clique para ir para a página do autor"
+    autorDoLivro.textContent = livro.autor
+
+    const generoDoLivro = document.createElement("span")
+    generoDoLivro.classList.add("genero")
+    generoDoLivro.textContent = livro.genero
+
+    const resumoDoLivro = document.createElement("span")
+    resumoDoLivro.classList.add("resumo")
+    resumoDoLivro.textContent = livro.descricaoCurta
+
+    const containerInformacaoReserva = document.createElement("section")
+    containerInformacaoReserva.classList.add("container-informacoes-reserva")
+
+    const containerStatusReserva = document.createElement("section")
+    containerStatusReserva.classList.add("container-status-reserva")
+
+    const statusReserva = document.createElement("span")
+    statusReserva.classList.add("status-reserva")
+    statusReserva.textContent = reserva.status
+
+    const horaLimiteRetirada = document.createElement("span")
+    horaLimiteRetirada.classList.add("hora-limite-retirada")
+    horaLimiteRetirada.textContent = "Você pode retirar este livro até a data: " + reserva.dataHoraLimiteFormatado
+
+    const numeroChamada = document.createElement("span")
+    numeroChamada.classList.add("numero-chamada")
+    numeroChamada.textContent = "N° de chamada: " + livro.numeroChamada
+
+    const botaoCancelarReserva = document.createElement("button")
+    botaoCancelarReserva.classList.add("botao-cancelar-reserva")
+    botaoCancelarReserva.textContent = "Cancelar"
+
+
+    main.appendChild(containerReserva)
+    containerReserva.append(containerCapa, containerInformacaoLivro)
+    containerCapa.appendChild(imagemCapa)   
+    containerInformacaoLivro.append(tituloDoLivro, containerAutorEGenero, resumoDoLivro, containerInformacaoReserva)
+    containerInformacaoReserva.append(containerStatusReserva, botaoCancelarReserva)
+    containerAutorEGenero.append(autorDoLivro, generoDoLivro)
+    containerStatusReserva.append(statusReserva, horaLimiteRetirada, numeroChamada)
     
 }
--- src/js-scripts/criar-minhas-reservas.js
+++ src/js-scripts/criar-minhas-reservas.js
@@ -3,20 +3,15 @@ import { criarMinhasReservas } from "../js-funcoes/funcao-criar-minhas-reservas.
 import { ClasseLivro } from "../js-classes/classe-livro.js";
 
 window.addEventListener("DOMContentLoaded", () => {
-  const livrosReservados =
-    JSON.parse(localStorage.getItem("lista de reservas")) || [];
   const leitor = ClasseLeitor.vetorLeitores.find(
     (l) => l.usuario === ClasseLeitor.leitorLogado
   );
 
-  livrosReservados.forEach((reserva) => {
-    if (leitor && leitor.id === reserva.idUsuario) {
-      const livro = ClasseLivro.vetorLivros.find(
-        (li) => li.tombo === reserva.tomboLivro
-      );
-      if (livro) {
-        criarMinhasReservas(livro, leitor, reserva);
-      }
+  leitor.minhasReservas.forEach(reserva => {
+    const livro = ClasseLivro.vetorLivros.find(li => li.tombo === reserva.tomboLivro)
+    if (livro) {
+      criarMinhasReservas(livro, leitor, reserva)
     }
-  });
+  })
+  
 });
