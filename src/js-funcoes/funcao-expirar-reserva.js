4dbc63abf15284eeb3bed138e93beff74e324274
Jesse-Cunha <jesse.delgado@hotmail.com>
2025-11-10 15:13:08 -0300

    Correção de bug na expiração de reservas.

--- src/js-funcoes/funcao-expirar-reserva.js
+++ src/js-funcoes/funcao-expirar-reserva.js
@@ -2,16 +2,19 @@ import { ClasseReserva } from "../js-classes/classe-reserva.js"
 import { obterDataHoraServidor } from "./funcoes-de-data-e-hora.js"
 import { ClasseLeitor } from "../js-classes/classe-leitor.js"
 
-export function expirarReserva(reserva){
+export async function expirarReserva(reserva){
 
+    
     const dataHoraLimite = reserva.dataHoraLimite
     
-    const agora = obterDataHoraServidor()
+    const agora = await obterDataHoraServidor()
+
+    const agoraMs = agora.getTime()
 
     //capta a reserva no vetor de todas as reservas, para alterar também essa lista no local storage
-    const reservaVetorReservas = ClasseReserva.vetorReservas.find(r => r.id = reserva.id)
+    const reservaVetorReservas = ClasseReserva.vetorReservas.find(r => r.idReserva = reserva.idReserva)
 
-    if (agora > dataHoraLimite){
+    if (agoraMs > dataHoraLimite){
 
         //atualiza a expiração da reserva no vetor de reservas do leitor
         reserva.expirou = true
