b4bdb053f1d5f5eab85068132ce5cf473db2becb
Danilo Brito <danilo.brito.1562234@sga.pucminas.br>
2025-11-13 21:51:50 -0300

    Main

--- src/js-funcoes/funcao-criar-minhas-reservas.js
+++ src/js-funcoes/funcao-criar-minhas-reservas.js
@@ -87,7 +87,7 @@ export async function criarMinhasReservas(livro, reserva) {
         botaoCancelarReserva.classList.add("botao-reserva-expirada")
         botaoCancelarReserva.style.cursor = "not-allowed"
         botaoCancelarReserva.title = "Esta reserva está expirada"
-        containerReserva.style.borderColor = "rgba(7, 8, 7, 0.3)";
+        containerReserva.style.borderColor = "rgba(7, 8, 7, 0.2)";
 
         statusReserva.style.backgroundColor = "var(--vermelho-alaranjado)"
     }
