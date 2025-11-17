f8c247be9b3baa4d397311ebafea68f273a498ce
Gustavo <gustavocastro777888@gmail.com>
2025-11-17 19:33:31 -0300

    editar perfil

--- src/js-scripts/editar-perfil.js
+++ src/js-scripts/editar-perfil.js
@@ -1,45 +1,75 @@
-const vetorLeitores = JSON.parse(localStorage.getItem("lista de leitores")) || []
-const leitorLogado = vetorLeitores.find((leitor) => leitor.usuario === localStorage.getItem("leitor logado"))
 
-console.log(vetorLeitores)
-console.log(leitorLogado)
+const getleitor = () => {
+    const vetorLeitores = JSON.parse(localStorage.getItem("lista de leitores")) || []
+    const leitorAtual = vetorLeitores.find((leitor) => leitor.usuario === localStorage.getItem("leitor logado"))
+
+    return leitorAtual
+}
+
+const preencherDados = (leitorLogado) => {
+    console.log("leitorLogado", leitorLogado)
+    const usuario = document.getElementById("usuario");
+    usuario.value = leitorLogado.usuario;
+    usuario.disabled = true
+
+    const nome = document.getElementById("nome");
+    nome.value = leitorLogado.nome;
+    nome.disabled = true
+
+    const sobrenome = document.getElementById("sobrenome");
+    sobrenome.value = leitorLogado.sobrenome;
+    sobrenome.disabled = true
+
+    const cpf = document.getElementById("cpf");
+    cpf.value = leitorLogado.cpf;
+    cpf.disabled = true
+
+    const telefone = document.getElementById("telefone");
+    telefone.value = leitorLogado.telefone;
+    telefone.disabled = true
+
+    const email = document.getElementById("email");
+    email.value = leitorLogado.email;
+    email.disabled = true
+
+    const cep = document.getElementById("cep");
+    cep.value = leitorLogado.cep;
+    cep.disabled = true
+
+    const rua = document.getElementById("rua");
+    rua.value = leitorLogado.rua;
+    rua.disabled = true
+
+    const numeroFachada = document.getElementById("numero-fachada");
+    numeroFachada.value = leitorLogado.numeroFachada;
+    numeroFachada.disabled = true
+
+    const bairro = document.getElementById("bairro");
+    bairro.value = leitorLogado.bairro;
+    bairro.disabled = true
+
+    const cidade = document.getElementById("cidade");
+    cidade.value = leitorLogado.cidade;
+    cidade.disabled = true
+
+    const estado = document.getElementById("estado");
+    estado.value = leitorLogado.estado;
+    estado.disabled = true
+
+    const senha = document.getElementById("senha");
+    senha.value = leitorLogado.senha;
+    senha.disabled = true
+}
+let usuarioPadrao = getleitor()
+console.log("usuarioPadrao", usuarioPadrao)
+preencherDados(usuarioPadrao)
+
+// const checkboxGenero1 = document.getElementById("checkbox-genero-1");
+// checkboxGenero1.value = leitorLogado.checkboxGenero1;
+// checkboxGenero1.disabled = true
 
 
-const usuario = document.getElementById("usuario");
-usuario.value = leitorLogado.usuario;
 
-const nome = document.getElementById("nome");
-nome.value = leitorLogado.nome;
-
-const sobrenome = document.getElementById("sobrenome");
-sobrenome.value = leitorLogado.sobrenome;
-
-const cpf = document.getElementById("cpf");
-cpf.value = leitorLogado.cpf;
-
-const telefone = document.getElementById("telefone");
-telefone.value = leitorLogado.telefone;
-
-const email = document.getElementById("email");
-email.value = leitorLogado.email;
-
-const cep = document.getElementById("cep");
-cep.value = leitorLogado.cep;
-
-const rua = document.getElementById("rua");
-rua.value = leitorLogado.rua;
-
-const numeroFachada = document.getElementById("numero-fachada");
-numeroFachada.value = leitorLogado.numeroFachada;
-
-const bairro = document.getElementById("bairro");
-bairro.value = leitorLogado.bairro;
-
-const cidade = document.getElementById("cidade");
-cidade.value = leitorLogado.cidade;
-
-const estado = document.getElementById("estado");
-estado.value = leitorLogado.estado;
 
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -56,19 +86,20 @@ document.addEventListener("DOMContentLoaded", () => {
         //console.log(slug);
     }
 
-
-    leitorLogado.meusGeneros.forEach((genero) => {
+    usuarioPadrao = getleitor()
+    usuarioPadrao.meusGeneros.forEach((genero) => {
         const slugGenero = slug(genero);
-        const favorito = document.getElementById(slugGenero);
+        // const favorito = document.getElementById(slugGenero);
+        const favorito = document.querySelector(`.${slugGenero}`).checked = true;
         console.log(favorito);
 
-        if (favorito) {
-            favorito.classList.add("selecionado");
-            favorito.style.backgroundColor = "var(--azul-colombia)"
-            console.log(`Marcado: #${slugGenero}`);
-        } else {
-            console.warn(`Elemento não encontrado: #${slugGenero}`);
-        }
+        // if (favorito) {
+        //     favorito.classList.add("selecionado");
+        //     favorito.style.backgroundColor = "var(--azul-colombia)"
+        //     console.log(`Marcado: #${slugGenero}`);
+        // } else {
+        //     console.warn(`Elemento não encontrado: #${slugGenero}`);
+        // }
     });
 
 
@@ -85,6 +116,15 @@ botaoEditar.addEventListener("click", function (evento) {
     botaoEditar.setAttribute("hidden", "")
     document.getElementById("editar-dados-pessoais-confirmar").removeAttribute("hidden")
     document.getElementById("editar-dados-pessoais-cancelar").removeAttribute("hidden")
+    document.getElementById("usuario").removeAttribute("disabled")
+    document.getElementById("nome").removeAttribute("disabled")
+    document.getElementById("sobrenome").removeAttribute("disabled")
+    document.getElementById("cpf").removeAttribute("disabled")
+    document.getElementById("telefone").removeAttribute("disabled")
+    document.getElementById("email").removeAttribute("disabled")
+
+
+
 })
 
 
@@ -96,7 +136,38 @@ cancelarEditar.addEventListener("click", function (evento) {
     document.getElementById("editar-dados-pessoais-lapis").removeAttribute("hidden")
     document.getElementById("editar-dados-pessoais-cancelar").setAttribute("hidden", "")
     document.getElementById("editar-dados-pessoais-confirmar").setAttribute("hidden", "")
+    //document.getElementById("usuario").setttribute("disabled")
+    document.getElementById("usuario").disabled = true
+    document.getElementById("nome").disabled = true
+    document.getElementById("sobrenome").disabled = true
+    document.getElementById("cpf").disabled = true
+    document.getElementById("telefone").disabled = true
+    document.getElementById("email").disabled = true
 })
+
+const confirmarEditar = document.getElementById("editar-dados-pessoais-confirmar");
+confirmarEditar.addEventListener("click", function (evento) {
+    evento.preventDefault()
+    const nome = document.getElementById("nome");
+    const sobrenome = document.getElementById("sobrenome");
+    const cpf = document.getElementById("cpf");
+    const telefone = document.getElementById("telefone");
+    const email = document.getElementById("email");
+
+
+    editarFormulario({
+        nome: nome.value,
+        sobrenome: sobrenome.value,
+        cpf: cpf.value,
+        telefone: telefone.value,
+        email: email.value,
+    })
+
+
+})
+
+
+
 
 
 
@@ -104,11 +175,33 @@ cancelarEditar.addEventListener("click", function (evento) {
 const botaoEditarEndereco = document.getElementById("editar-endereco-lapis");
 
 
+const editarFormulario = (camposEditados) => {
+    const leitorAntigo = getleitor()
+    const usuarioEditado = { ...leitorAntigo, ...camposEditados }
+    const listaDeLeitores = [usuarioEditado]
+    localStorage.setItem("lista de leitores", JSON.stringify(listaDeLeitores));
+    preencherDados(usuarioEditado)
+}
+
+
 botaoEditarEndereco.addEventListener("click", function (evento) {
     evento.preventDefault()
     botaoEditarEndereco.setAttribute("hidden", "")
     document.getElementById("editar-endereco-confirmar").removeAttribute("hidden")
     document.getElementById("editar-endereco-cancelar").removeAttribute("hidden")
+    document.getElementById("cep").removeAttribute("disabled")
+    document.getElementById("rua").removeAttribute("disabled")
+    document.getElementById("numero-fachada").removeAttribute("disabled")
+    document.getElementById("bairro").removeAttribute("disabled")
+    document.getElementById("cidade").removeAttribute("disabled")
+    document.getElementById("estado").removeAttribute("disabled")
+
+    // const leitorAntigo = getleitor()
+    // const usuarioEditado = { ...leitorAntigo, nome: "gustavo" }
+    // const listaDeLeitores = [usuarioEditado]
+    // localStorage.setItem("lista de leitores", JSON.stringify(listaDeLeitores));
+    // preencherDados(usuarioEditado)
+
 })
 
 
@@ -120,9 +213,47 @@ cancelarEditarEndereco.addEventListener("click", function (evento) {
     document.getElementById("editar-endereco-lapis").removeAttribute("hidden")
     document.getElementById("editar-endereco-cancelar").setAttribute("hidden", "")
     document.getElementById("editar-endereco-confirmar").setAttribute("hidden", "")
+    document.getElementById("cep").disabled = true
+    document.getElementById("rua").disabled = true
+    document.getElementById("numero-fachada").disabled = true
+    document.getElementById("bairro").disabled = true
+    document.getElementById("cidade").disabled = true
+    document.getElementById("estado").disabled = true
 })
 
 
+const confirmarEditarEndereco = document.getElementById("editar-endereco-confirmar");
+confirmarEditarEndereco.addEventListener("click", function (evento) {
+    evento.preventDefault()
+    const cep = document.getElementById("cep");
+    const rua = document.getElementById("rua");
+    const numeroFachada = document.getElementById("numero-fachada");
+    const bairro = document.getElementById("bairro");
+    const cidade = document.getElementById("cidade");
+    const estado = document.getElementById("estado");
+
+
+    editarFormulario({
+        cep: cep.value,
+        rua: rua.value,
+        numeroFachada: numeroFachada.value,
+        bairro: bairro.value,
+        cidade: cidade.value,
+        estado: estado.value,
+    })
+
+
+})
+
+
+document.getElementById("checkbox-genero-1").disabled = true;
+document.getElementById("checkbox-genero-2").disabled = true;
+document.getElementById("checkbox-genero-3").disabled = true;
+document.getElementById("checkbox-genero-4").disabled = true;
+document.getElementById("checkbox-genero-5").disabled = true;
+document.getElementById("checkbox-genero-6").disabled = true;
+document.getElementById("checkbox-genero-7").disabled = true;
+document.getElementById("checkbox-genero-8").disabled = true;
 
 
 
@@ -135,6 +266,16 @@ botaoEditarFavoritos.addEventListener("click", function (evento) {
     botaoEditarFavoritos.setAttribute("hidden", "")
     document.getElementById("editar-favoritos-confirmar").removeAttribute("hidden")
     document.getElementById("editar-favoritos-cancelar").removeAttribute("hidden")
+
+    document.getElementById("checkbox-genero-1").disabled = false;
+    document.getElementById("checkbox-genero-2").disabled = false;
+    document.getElementById("checkbox-genero-3").disabled = false;
+    document.getElementById("checkbox-genero-4").disabled = false;
+    document.getElementById("checkbox-genero-5").disabled = false;
+    document.getElementById("checkbox-genero-6").disabled = false;
+    document.getElementById("checkbox-genero-7").disabled = false;
+    document.getElementById("checkbox-genero-8").disabled = false;
+
 })
 
 
@@ -146,8 +287,73 @@ cancelarEditarFavoritos.addEventListener("click", function (evento) {
     document.getElementById("editar-favoritos-lapis").removeAttribute("hidden")
     document.getElementById("editar-favoritos-cancelar").setAttribute("hidden", "")
     document.getElementById("editar-favoritos-confirmar").setAttribute("hidden", "")
+    document.getElementById("checkbox-genero-1").disabled = true;
+    document.getElementById("checkbox-genero-2").disabled = true;
+    document.getElementById("checkbox-genero-3").disabled = true;
+    document.getElementById("checkbox-genero-4").disabled = true;
+    document.getElementById("checkbox-genero-5").disabled = true;
+    document.getElementById("checkbox-genero-6").disabled = true;
+    document.getElementById("checkbox-genero-7").disabled = true;
+    document.getElementById("checkbox-genero-8").disabled = true;
 })
 
+
+const editarFavoritosConfirmar = document.getElementById("editar-favoritos-confirmar");
+
+
+editarFavoritosConfirmar.addEventListener("click", function (evento) {
+    evento.preventDefault()
+    botaoEditarFavoritos.setAttribute("hidden", "")
+    document.getElementById("editar-favoritos-lapis").removeAttribute("hidden")
+    document.getElementById("editar-favoritos-cancelar").setAttribute("hidden", "")
+    document.getElementById("editar-favoritos-confirmar").setAttribute("hidden", "")
+
+    document.getElementById("checkbox-genero-1").disabled = true;
+    document.getElementById("checkbox-genero-2").disabled = true;
+    document.getElementById("checkbox-genero-3").disabled = true;
+    document.getElementById("checkbox-genero-4").disabled = true;
+    document.getElementById("checkbox-genero-5").disabled = true;
+    document.getElementById("checkbox-genero-6").disabled = true;
+    document.getElementById("checkbox-genero-7").disabled = true;
+    document.getElementById("checkbox-genero-8").disabled = true;
+    const generos = []
+    if (document.getElementById("checkbox-genero-1").checked) {
+        generos.push("Divulgação científica")
+    }
+    if (document.getElementById("checkbox-genero-2").checked) {
+        generos.push("Filosofia")
+    }
+
+    if (document.getElementById("checkbox-genero-3").checked) {
+        generos.push("Mistério")
+    }
+
+    if (document.getElementById("checkbox-genero-4").checked) {
+        generos.push("Psicologia")
+    }
+
+    if (document.getElementById("checkbox-genero-5").checked) {
+        generos.push("Romance")
+    }
+
+    if (document.getElementById("checkbox-genero-6").checked) {
+        generos.push("Sociologia")
+    }
+
+    if (document.getElementById("checkbox-genero-7").checked) {
+        generos.push("Ficção científica")
+    }
+
+    if (document.getElementById("checkbox-genero-8").checked) {
+        generos.push("Fantasia")
+    }
+
+
+    editarFormulario({
+        meusGeneros: generos,
+    })
+
+})
 
 
 
@@ -161,8 +367,7 @@ botaoEditarSenha.addEventListener("click", function (evento) {
     document.getElementById("editar-senha-confirmar").removeAttribute("hidden")
     document.getElementById("editar-senha-cancelar").removeAttribute("hidden")
     document.getElementById("editar-senha-confirmar").removeAttribute("hidden")
-    document.getElementById("nova-senha").removeAttribute("hidden")
-    document.getElementById("confirmar-nova-senha").removeAttribute("hidden")
+    document.getElementById("quadro-senhas").style.display = "flex";
 
 
 })
@@ -176,4 +381,32 @@ cancelarEditarSenha.addEventListener("click", function (evento) {
     document.getElementById("editar-senha-lapis").removeAttribute("hidden")
     document.getElementById("editar-senha-cancelar").setAttribute("hidden", "")
     document.getElementById("editar-senha-confirmar").setAttribute("hidden", "")
+    document.getElementById("quadro-senhas").style.display = "none";
+    document.getElementById("senha").disabled = true
+
+})
+
+
+
+const confirmarEditarSenha = document.getElementById("editar-senha-confirmar");
+confirmarEditarSenha.addEventListener("click", function (evento) {
+    evento.preventDefault()
+    const senha = document.getElementById("senha");
+    const novaSenha = document.getElementById("nova-senha");
+    const confirmarNovaSenha = document.getElementById("confirmar-nova-senha");
+
+    console.log("novaSenha.value", novaSenha.value)
+    console.log("confirmarEditarSenha.value", confirmarEditarSenha.value)
+
+    if (novaSenha.value === confirmarNovaSenha.value) {
+        editarFormulario({
+            senha: confirmarNovaSenha.value,
+        })
+    } else (
+        alert("As senhas devem ser digitadas iguais")
+    )
+
+
+
+
 })
