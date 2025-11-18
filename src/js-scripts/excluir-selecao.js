20ca29b9490073d57f8fcff1d85bf7316e66239c
Danilo Brito <danilo.brito.1562234@sga.pucminas.br>
2025-11-18 08:21:39 -0300

    Inicio da funcionalidade da exclusão de seleção

---
+++ src/js-scripts/excluir-selecao.js
@@ -0,0 +1,16 @@
+import { removerSelecao } from "../js-funcoes/funcao-remover-selecao.js"
+
+document.addEventListener("click", (e) => {
+
+  const lixeira = e.target.closest("button.btn-trash")
+  if (!lixeira) return
+
+  e.preventDefault()
+  
+  const isbn = lixeira.id.split("-")[1]
+
+  removerSelecao(isbn)
+
+  location.reload()
+
+})
