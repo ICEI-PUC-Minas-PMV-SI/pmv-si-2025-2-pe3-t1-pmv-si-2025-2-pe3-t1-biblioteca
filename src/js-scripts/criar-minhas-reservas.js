eedd5192521c0348e1aa2ad33c2f7b46043762e9
Jesse-Cunha <jesse_cunha@outlook.com.br>
2025-12-02 17:02:48 -0300

    Modificação do carregamento da página "Minhas reservas" para garantir ordem das reservas sempre igual. Reservas mais recentes ficam no topo da página.

--- src/js-scripts/criar-minhas-reservas.js
+++ src/js-scripts/criar-minhas-reservas.js
@@ -2,16 +2,19 @@ import { ClasseLeitor } from "../js-classes/classe-leitor.js";
 import { criarMinhasReservas } from "../js-funcoes/funcao-criar-minhas-reservas.js";
 import { ClasseLivro } from "../js-classes/classe-livro.js";
 
-window.addEventListener("DOMContentLoaded", () => {
+// modifiquei o código para se adequar à assincronicidade da função `criarMinhasReservas`
+window.addEventListener("DOMContentLoaded", async () => {
   const leitor = ClasseLeitor.vetorLeitores.find(
     (l) => l.usuario === ClasseLeitor.leitorLogado
-  );
+  )
 
-  leitor.minhasReservas.forEach(reserva => {
+  // garante que as reservas mais recentes estejam no topo da página com o método reverse(), que inverte a ordem das reservas realizadas pelo leitor
+  for (const reserva of leitor.minhasReservas.reverse()) {
     const livro = ClasseLivro.vetorLivros.find(li => li.tombo === reserva.tomboLivro)
     if (livro) {
-      criarMinhasReservas(livro, reserva)
+      await criarMinhasReservas(livro, reserva)
     }
-  })
+  }
+
+})
   
-});
